afab341f033eb3ae9ed2d33a06e86d68450a9e69
Hagiwara <hagiyoshihep@gmail.com>
2018-09-20 17:14:20 +0900

    logr calculation <- failed need logS

--- Odderon_BK/RK_BK_including_odderon.cpp
+++ Odderon_BK/RK_BK_including_odderon.cpp
@@ -38,7 +38,7 @@ using namespace std;
 
 #define LARGE_Nc
 
-//#define LOGSCALE
+#define LOGSCALE
 
 //for DE formula
 int nfunc;
@@ -67,8 +67,8 @@ const double IMPACTP_B = 1.0;
 * The evolution step size.
 */
 #define DELTA_T         0.01
-#define OUTPUT_DELTA_T  0.1
-#define END_T           5.0
+#define OUTPUT_DELTA_T  0.01
+#define END_T           0.1
 
 
 /**
@@ -112,7 +112,7 @@ void init_BK_log(std::complex<double>* Smatrix_in) {
 	double h_theta = 2.0*Pi / NPHI;
 	double   xmax = h * NX / 1.0, xmin = -h * NX / 2.0, ymin = 0.0;
 	double   *x = new double[N*N], *y = new double[N*N];
-	for (int j = 0; j < NX; j++) {
+	for (int j = 0; j < NPHI; j++) {
 		for (int i = 0; i < NX; i++)
 		{
 			x[NX*j + i] = xmin + i * h;
@@ -132,12 +132,12 @@ void init_BK_log(std::complex<double>* Smatrix_in) {
 
 void Integration_in_BK_equation(std::complex<double>* Smatrix_in, std::complex<double>* Integrated_out);
 
+
 void f_one_step_BK_complex(
 	vector<complex<double>> &sol_BMS_g_in,
 	vector<complex<double>> &NEW_BMS_g_in, const double dtau)
 {
-	vector<complex<double>> temp_s_BMS_in(NX*NX, 0),
-		temp_s_K1_in(NX*NX, 0);
+	vector<complex<double>> temp_s_BMS_in(NX*NX, 0);
 
 #pragma omp parallel for num_threads(6)
 	for (int veint = 0; veint < NX*NX; veint++) {
@@ -188,6 +188,62 @@ void one_step_complex(vector<complex<double>> &sol_BK, double dtau){
 	}
 }
 
+void Integration_in_logscale_BK_equation(std::complex<double>* Smatrix_in, std::complex<double>* Integrated_out);
+
+void f_one_step_logscale_BK_complex(
+	vector<complex<double>> &sol_BMS_g_in,
+	vector<complex<double>> &NEW_BMS_g_in, const double dtau)
+{
+	vector<complex<double>> temp_s_BMS_in(NX*NPHI, 0);
+
+#pragma omp parallel for num_threads(6)
+	for (int veint = 0; veint < NX*NPHI; veint++) {
+		temp_s_BMS_in[veint] = sol_BMS_g_in[veint];
+	}
+
+	Integration_in_logscale_BK_equation(temp_s_BMS_in.data(), NEW_BMS_g_in.data());
+
+}
+
+void s_one_step_logscale_BK_complex(const vector<complex<double>> &K1_g_in,
+	vector<complex<double>> &sol_BMS_g_in,
+	vector<complex<double>> &NEW_BMS_g_in, const double dtau)
+{
+	vector<complex<double>> temp_s_BMS_in(NX*NPHI, 0);
+
+#pragma omp parallel for num_threads(6)
+	for (int veint = 0; veint < NX*NPHI; veint++) {
+		temp_s_BMS_in[veint] = sol_BMS_g_in[veint] + dtau / 2.0*K1_g_in[veint];
+	}
+
+	Integration_in_logscale_BK_equation(temp_s_BMS_in.data(), NEW_BMS_g_in.data());
+
+}
+
+void one_step_logscale_complex(vector<complex<double>> &sol_BK, double dtau) {
+
+
+	vector<complex<double>> K1_g_comp(NX*NPHI, 0);
+	vector<complex<double>> K2_g_comp(NX*NPHI, 0);
+	vector<complex<double>> K3_g_comp(NX*NPHI, 0);
+	vector<complex<double>> K4_g_comp(NX*NPHI, 0);
+
+	//evolve using the BK equation
+	f_one_step_logscale_BK_complex(sol_BK, K1_g_comp, dtau);
+	s_one_step_logscale_BK_complex(K1_g_comp, sol_BK, K2_g_comp, dtau);
+	s_one_step_logscale_BK_complex(K2_g_comp, sol_BK, K3_g_comp, dtau);
+	s_one_step_logscale_BK_complex(K3_g_comp, sol_BK, K4_g_comp, 2.0*dtau);
+
+	tau += dtau;
+	vector<complex<double>> Old_BK_in(NX*NPHI, 0);
+	Old_BK_in = sol_BK;
+
+#pragma omp parallel for num_threads(6)
+	for (int i = 0; i < NX*NPHI; ++i) {
+		sol_BK[i] = Old_BK_in[i]
+			+ dtau / 6.0*(K1_g_comp[i] + 2.0*K2_g_comp[i] + 2.0*K3_g_comp[i] + K4_g_comp[i]);
+	}
+}
 
 /**
 * Prints g_Y.
@@ -236,21 +292,89 @@ void print_g(vector<complex<double>> &sol_BK) {
 }
 
 
+/**
+* Prints g_Y.
+*/
+void print_logscale_g(vector<complex<double>> &sol_BK) {
+	int N = NX;
+	double h = 1.0*LATTICE_SIZE / NX;
+	double h_theta = 2.0*Pi / NPHI;
+	double   xmax = h * NX / 1.0, xmin = -h * NX / 2.0, ymin = 0.0;
+	double   *x = new double[N*NPHI], *y = new double[N*NPHI];
+	for (int j = 0; j < NPHI; j++) {
+		for (int i = 0; i < NX; i++)
+		{
+			x[NX*j + i] = xmin + i * h;
+			y[NX*j + i] = ymin + j * h_theta;
+		}
+	}
+
+	ostringstream ofilename, ofilename2;
+	ofilename << "G:\\hagiyoshi\\Data\\BK_odderon\\solutions\\BK_logscale_res_size"
+		<< LATTICE_SIZE << "_grid_" << NX << "_phi_" << NPHI << "_timestep_" << DELTA_T << "_t_" << tau << "_hipre.txt";
+	ofstream ofs_res(ofilename.str().c_str());
+	ofilename2 << "G:\\hagiyoshi\\Data\\BK_odderon\\solutions\\BK_logscale_res_distance_size"
+		<< LATTICE_SIZE << "_grid_" << NX << "_phi_" << NPHI << "_timestep_" << DELTA_T << "_t_" << tau << "_hipre.txt";
+	ofstream ofs_res2(ofilename2.str().c_str());
+
+	ofs_res << "# r \t phi \t Re( S ) \t Im( S )" << endl;
+	ofs_res2 << "# r pi/2 \t Re( S ) \t Im( S )" << endl;
+
+	for (int i = 0; i < NPHI; i++) {
+		for (int j = 0; j < NX; j++) {
+			ofs_res << scientific << exp(x[NX*i + j]) << "\t" << y[NX*i + j] << "\t"
+				<< sol_BK[NX*i + j].real() << "\t" << sol_BK[NX*i + j].imag() << endl;
+		}
+
+		ofs_res << endl;
+	}
+
+	for (int i = 0; i < N; i++) {
+
+		ofs_res2 << scientific << exp(x[NX*NPHI/4 + i]) << "\t"
+			<< sol_BK[NX*NPHI / 4 + i].real() << "\t" << sol_BK[NX*NPHI / 4 + i].imag() << endl;
+	}
+
+	delete[](x);
+	delete[](y);
+}
+
 
 int main(){
 	time_t t0 = time(NULL);
+	ostringstream ofilename;
+#ifdef LOGSCALE
+	ofilename << "G:\\hagiyoshi\\Data\\BK_odderon\\solutions\\BK_logscale_res_taudep_size"
+		<< LATTICE_SIZE << "_grid_" << NX << "_phi_" << NPHI << "_timestep_" << DELTA_T << "_hipre.txt";
+#else
+	ofilename << "G:\\hagiyoshi\\Data\\BK_odderon\\solutions\\BK_res_taudep_size"
+		<< LATTICE_SIZE << "_grid_" << NX << "_timestep_" << DELTA_T << "_hipre.txt";
+#endif
+	ofstream ofs_res(ofilename.str().c_str());
 
+	ofs_res << "# tau \t r \t Re( S ) \t Im( S )" << endl;
 
+#ifdef LOGSCALE
+	vector<complex<double>> sol_BK_comp(NX*NPHI, 0);
+	double h_phi = 2.0*Pi / NPHI;
+	double h_half = 1.0*LATTICE_SIZE / NX;
+#else
+	double h_half = 1.0*LATTICE_SIZE / NX;
 	vector<complex<double>> sol_BK_comp(NX*NX, 0);
+#endif
    
         
 	const double EPS = 1e-12;
 
 	double next_tau = 0;
 
+#ifdef LOGSCALE
+	init_BK_log(sol_BK_comp.data());
+	print_logscale_g(sol_BK_comp);
+#else
 	init_BK(sol_BK_comp.data());
-
 	print_g(sol_BK_comp);
+#endif
 
 	//evaluate the number of time step
 	for (;;) {
@@ -260,13 +384,36 @@ int main(){
 		}
 		next_tau = min(next_tau + OUTPUT_DELTA_T, END_T);
 		while (tau < next_tau - EPS) {
-			one_step_complex(sol_BK_comp,min(DELTA_T, next_tau - tau));
+#ifdef LOGSCALE
+			one_step_logscale_complex(sol_BK_comp,min(DELTA_T, next_tau - tau));
+#else
+			one_step_complex(sol_BK_comp, min(DELTA_T, next_tau - tau));
+#endif
 		}
-		print_g(sol_BK_comp);
+#ifdef LOGSCALE
+		print_logscale_g(sol_BK_comp);
+		ofs_res << scientific << tau << "\t" << exp( 3.0 * NX / 4.0*h_half) << "\t"
+			<< sol_BK_comp[NPHI / 4 * NX + 3 * NX / 4].real() << "\t" << sol_BK_comp[NPHI / 4 * NX + 3 * NX / 4].imag() << "\n";
 		cout << "time \t" << tau << "\n";
+#else
+		print_g(sol_BK_comp);
+		ofs_res << scientific << tau<< "\t" << sqrt(2.0)*h_half*(double)NX/4.0 << "\t"
+			<< sol_BK_comp[3*NX / 4 * NX + 3 * NX / 4].real() << "\t" << sol_BK_comp[3 * NX / 4 * NX + 3 * NX / 4].imag() << "\n";
+		cout << "time \t" << tau << "\n";
+#endif
 	}
 
+#ifdef LOGSCALE
+	print_logscale_g(sol_BK_comp);
+	ofs_res << scientific << tau << "\t" << exp(3.0 * NX / 4.0*h_half) << "\t"
+		<< sol_BK_comp[NPHI / 4 * NX + 3 * NX / 4].real() << "\t" << sol_BK_comp[NPHI / 4 * NX + 3 * NX / 4].imag() << "\n";
+	cout << "time \t" << tau << "\n";
+#else
 	print_g(sol_BK_comp);
+	ofs_res << scientific << tau << "\t" << sqrt(2.0)*h_half*(double)NX / 4.0 << "\t"
+		<< sol_BK_comp[3 * NX / 4 * NX + 3 * NX / 4].real() << "\t" << sol_BK_comp[3 * NX / 4 * NX + 3 * NX / 4].imag() << "\n";
+	cout << "time \t" << tau << "\n";
+#endif
 
 	time_t t1 = time(NULL);
 	cout << t0 - t1 << endl;
--- Odderon_BK/plane_integration.cu
+++ Odderon_BK/plane_integration.cu
@@ -22,6 +22,96 @@
 
 #include "Parameters.h"
 
+//	Returns of acos
+//		Result will be in radians, in the interval[0, ƒÎ] for x inside[-1, +1].
+//		acos(1) returns + 0.
+//		acos(x) returns NaN for x outside[-1, +1].
+
+//input S_matrix NX*NPHI, x_1 NX, p_1 PHI;
+__device__ cuDoubleComplex linear_interpolation_Smatrix(cuDoubleComplex* S_matrix,
+	double* x_1, double* p_1,double x,double p)
+{
+	double h = 1.0*LATTICE_SIZE / NX;
+	double h_theta = 2.0*Pi / NPHI;
+	double   xmax = h * NX / 1.0, xmin = -h * NX / 2.0, ymin = 0.0;
+	int i_x=0;
+	int i_p = 0;
+
+	for (int i=0;i<NX-1;i++)
+	{
+		if (x < x_1[i]) { i_x = i; }
+	}
+
+	for (int i = 0; i < NPHI-1; i++)
+	{
+		if (p < p_1[NX*i] ) { i_p = i; }
+	}
+
+	if (x > xmax || p > 2.0*Pi || x < xmin || p < ymin)
+	{
+		printf( "out of range \n");
+		assert(1);
+	}
+
+	//bilinear interpolation
+	double t = (x - x_1[i_x]) / (x_1[i_x + 1] - x_1[i_x]);
+	double u = (p - p_1[NX*i_p]) / (p_1[NX*(i_p + 1)] - p_1[NX*i_p]);
+
+	cuDoubleComplex interpolate;
+
+	interpolate = make_cuDoubleComplex(
+		(1.0 - t)*(1.0 - u)*S_matrix[i_p*NX + i_x].x + t * (1.0 - u)*S_matrix[i_p*NX + i_x + 1].x
+		+ t * u*S_matrix[(i_p + 1)*NX + i_x + 1].x + (1.0 - t)*u*S_matrix[(i_p + 1)*NX + i_x].x   ,
+		(1.0 - t)*(1.0 - u)*S_matrix[i_p*NX + i_x].y + t * (1.0 - u)*S_matrix[i_p*NX + i_x + 1].y
+		+ t * u*S_matrix[(i_p + 1)*NX + i_x + 1].y + (1.0 - t)*u*S_matrix[(i_p + 1)*NX + i_x].y
+	);
+
+	return interpolate;
+}
+
+
+//input S_matrix NX*NPHI, x_1 NX, p_1 PHI;
+__device__ cuDoubleComplex linear_interpolation_Smatrix_test(cuDoubleComplex* S_matrix,
+	double* x_1, double* p_1, double x, double p)
+{
+	double h = 1.0*LATTICE_SIZE / NX;
+	double h_theta = 2.0*Pi / NPHI;
+	double   xmax = h * NX / 1.0, xmin = -h * NX / 2.0, ymin = 0.0;
+	int i_x = 0;
+	int i_p = 0;
+
+	for (int i = 0; i < NX - 1; i++)
+	{
+		if (x < x_1[i]) { i_x = i; }
+	}
+
+	for (int i = 0; i < NPHI - 1; i++)
+	{
+		if (p < p_1[NX*i]) { i_p = i; }
+	}
+
+	if (x > xmax || p > 2.0*Pi || x < xmin || p < ymin)
+	{
+		printf("out of range \n");
+		assert(1);
+	}
+
+	//bilinear interpolation
+	double t = (x - x_1[i_x]) / (x_1[i_x + 1] - x_1[i_x]);
+	double u = (p - p_1[NX*i_p]) / (p_1[NX*(i_p + 1)] - p_1[NX*i_p]);
+
+	cuDoubleComplex interpolate;
+
+	interpolate = make_cuDoubleComplex(
+		(1.0 - t)*(1.0 - u)*S_matrix[i_p*NX + i_x].x + t * (1.0 - u)*S_matrix[i_p*NX + i_x + 1].x
+		+ t * u*S_matrix[(i_p + 1)*NX + i_x + 1].x + (1.0 - t)*u*S_matrix[(i_p + 1)*NX + i_x].x,
+		(1.0 - t)*(1.0 - u)*S_matrix[i_p*NX + i_x].y + t * (1.0 - u)*S_matrix[i_p*NX + i_x + 1].y
+		+ t * u*S_matrix[(i_p + 1)*NX + i_x + 1].y + (1.0 - t)*u*S_matrix[(i_p + 1)*NX + i_x].y
+	);
+
+	return interpolate;
+}
+
 
 __global__ void Integration_BK_direct(cuDoubleComplex* integrated, cuDoubleComplex* S_matrix,
 	double* x_1, double* y_1, double h, int N_ini, int N_las, int N) {
@@ -108,6 +198,199 @@ __global__ void Integration_BK_direct(cuDoubleComplex* integrated, cuDoubleCompl
 }
 
 
+__global__ void Integration_BK_logscale_direct(cuDoubleComplex* integrated, cuDoubleComplex* S_matrix,
+	double* x_1, double* y_1, double h, int N) {
+
+	int i = threadIdx.x + blockIdx.x*blockDim.x;
+	int j = threadIdx.y + blockIdx.y*blockDim.y;
+	int index = j * N + i;
+	if (i < N && j < NPHI) {
+		integrated[index] = make_cuDoubleComplex(0.0, 0.0);
+		//sit the index which is center of the gaussian.
+
+		double   xmax = h * NX / 1.0, xmin = -h * NX / 2.0, ymin = 0.0;
+		double h_theta = 2.0*Pi / NPHI;
+		cuDoubleComplex complex_zero = make_cuDoubleComplex(0.0, 0.0);
+		//If x=N*j+i, then -x=N*(N-j)+N-i(when the origin is x= N*N/2 + N/2).
+		for (int m = 0; m < NPHI; m++) {
+			for (int n = 0; n < N; n++) {
+				double simpson1 = 1.0;
+				double simpson2 = 1.0;
+				int diffinitm = m - 0;
+
+
+				if (n == 0 || n == N - 1) {
+					simpson2 = 1.0 / 3.0;
+				}
+				else if (n % 2 == 0) {
+					simpson2 = 2.0 / 3.0;
+				}
+				else {
+
+					simpson2 = 4.0 / 3.0;
+				}
+
+				cuDoubleComplex trV_V = make_cuDoubleComplex(0.0, 0.0);
+				double r_z = 1.0 / 2.0*log(exp(2.0*x_1[j * N + i]) + exp(2.0*x_1[m * N + n])
+											- 2.0*exp(x_1[j * N + i] + x_1[m * N + n])*cos(y_1[j * N + i] - y_1[m * N + n]));
+				double r_z2 = exp(2.0*x_1[j * N + i]) + exp(2.0*x_1[m * N + n])
+					- 2.0*exp(x_1[j * N + i] + x_1[m * N + n])*cos(y_1[j * N + i] - y_1[m * N + n]);
+				double angletocos = (exp(x_1[j * N + i])*cos(y_1[j * N + i]) - exp(x_1[m * N + n]) * cos(y_1[m * N + n])) / sqrt(r_z2);
+				if (angletocos >= 1.0) {
+					angletocos = 1.0;
+				}
+				else if (angletocos <= -1.0) {
+					angletocos = -1.0;
+				}
+				//if r-z is out of the region then we take the S(r-z) =0.
+				if (r_z < xmin || r_z > xmax - h) {
+					//trV= - S(r)
+					trV_V = cuCsub(trV_V,
+						S_matrix[j * N + i]);
+				}
+				else {
+				//	Returns of acos
+				//		Result will be in radians, in the interval[0, ƒÎ] for x inside[-1, +1].
+				//		acos(1) returns + 0.
+				//		acos(x) returns NaN for x outside[-1, +1].
+
+					//trV=S(r-z)
+					trV_V = cuCadd(trV_V,
+						linear_interpolation_Smatrix(S_matrix,x_1,y_1,r_z,
+								acos(angletocos) )
+					);
+					//trV=S(r-z)*S(-z) <- S(-x) = S(x)^*
+					trV_V = cuCmul(trV_V,
+						cuConj(S_matrix[m * N + n]));
+					//trV=S(r-z)*S(-z) - S(r)
+					trV_V = cuCsub(trV_V,
+						S_matrix[j * N + i]);
+				}
+				//Caution!!! nan * 0 = nan
+				if ((x_1[j * N + i] - x_1[m * N + n])*(x_1[j * N + i] - x_1[m * N + n]) < 1.0e-10 &&
+					(y_1[j * N + i] - y_1[m * N + n])*(y_1[j * N + i] - y_1[m * N + n]) < 1.0e-10 || r_z2<0.0) {
+					trV_V = make_cuDoubleComplex(
+						0.0
+						,
+						0.0
+					);
+				}
+
+				cuDoubleComplex coeff = make_cuDoubleComplex(
+					simpson1*simpson2
+					*exp(2.0*x_1[j*N + i] - x_1[m * N + n])
+					/ r_z2,
+					0.0
+				);
+
+				if ((x_1[j * N + i] - x_1[m * N + n])*(x_1[j * N + i] - x_1[m * N + n]) < 1.0e-10 &&
+					(y_1[j * N + i] - y_1[m * N + n])*(y_1[j * N + i] - y_1[m * N + n]) < 1.0e-10) {
+					coeff = make_cuDoubleComplex(
+						0.0
+						,
+						0.0
+					);
+				}
+
+				integrated[index] = cuCadd(integrated[index], cuCmul(coeff, trV_V));
+
+			}
+		}
+
+		cuDoubleComplex coeff2 = make_cuDoubleComplex(h*h_theta*ALPHA_S_BAR / 2.0 / Pi, 0.0);
+		
+		integrated[index] = cuCmul(integrated[index], coeff2);
+	}
+}
+
+
+__global__ void Integration_BK_logscale_direct_test(cuDoubleComplex* integrated, cuDoubleComplex* S_matrix,
+	double* x_1, double* y_1, double h, int N) {
+
+	int i = threadIdx.x + blockIdx.x*blockDim.x;
+	int j = threadIdx.y + blockIdx.y*blockDim.y;
+	int index = j * N + i;
+	if (i < N && j < NPHI) {
+		integrated[index] = make_cuDoubleComplex(0.0, 0.0);
+		//sit the index which is center of the gaussian.
+
+		double   xmax = h * NX / 1.0, xmin = -h * NX / 2.0, ymin = 0.0;
+		double h_theta = 2.0*Pi / NPHI;
+		cuDoubleComplex complex_zero = make_cuDoubleComplex(0.0, 0.0);
+		//If x=N*j+i, then -x=N*(N-j)+N-i(when the origin is x= N*N/2 + N/2).
+		for (int m = 0; m < NPHI; m++) {
+			for (int n = 0; n < N; n++) {
+				double simpson1 = 1.0;
+				double simpson2 = 1.0;
+
+
+				cuDoubleComplex trV_V = make_cuDoubleComplex(1.0, 0.0);
+				
+
+				double r_z = 1.0 / 2.0*log(exp(2.0*x_1[j * N + i]) + exp(2.0*x_1[m * N + n])
+					- 2.0*exp(x_1[j * N + i] + x_1[m * N + n])*cos(y_1[j * N + i] - y_1[m * N + n]));
+				double r_z2 = exp(2.0*x_1[j * N + i]) + exp(2.0*x_1[m * N + n])
+					- 2.0*exp(x_1[j * N + i] + x_1[m * N + n])*cos(y_1[j * N + i] - y_1[m * N + n]);
+				if (r_z < xmin || r_z > xmax - h) {
+					//trV= - S(r)
+					trV_V = cuCsub(trV_V,
+						S_matrix[j * N + i]);
+				}
+				else {
+					//trV=S(r-z)
+					//trV_V = cuCadd(trV_V,
+					//	linear_interpolation_Smatrix(S_matrix, x_1, y_1, 2.0,
+					//		Pi/4.0)
+					//); 
+					trV_V = cuCadd(trV_V,
+						linear_interpolation_Smatrix_test(S_matrix, x_1, y_1, r_z,
+							acos(30.0 ))
+					);
+					//trV=S(r-z)*S(-z) <- S(-x) = S(x)^*
+					trV_V = cuCmul(trV_V,
+						cuConj(S_matrix[m * N + n]));
+					//trV=S(r-z)*S(-z) - S(r)
+					trV_V = cuCsub(trV_V,
+						S_matrix[j * N + i]);
+				}
+
+				if ((x_1[j * N + i] - x_1[m * N + n])*(x_1[j * N + i] - x_1[m * N + n]) < 1.0e-10 &&
+					(y_1[j * N + i] - y_1[m * N + n])*(y_1[j * N + i] - y_1[m * N + n]) < 1.0e-10 || r_z2 < 0.0) {
+					trV_V = make_cuDoubleComplex(
+						0.0
+						,
+						0.0
+					);
+				}
+
+
+				cuDoubleComplex coeff = make_cuDoubleComplex(
+					simpson1*simpson2
+					*exp(2.0*x_1[j*N + i] - x_1[m * N + n])/r_z2
+					,
+					0.0
+				);
+
+				if ((x_1[j * N + i] - x_1[m * N + n])*(x_1[j * N + i] - x_1[m * N + n]) < 1.0e-10 &&
+					(y_1[j * N + i] - y_1[m * N + n])*(y_1[j * N + i] - y_1[m * N + n]) < 1.0e-10) {
+					coeff = make_cuDoubleComplex(
+						0.0
+						,
+						0.0
+					);
+				}
+				integrated[index] = cuCadd(integrated[index], cuCmul(coeff, trV_V));
+
+			}
+		}
+		cuDoubleComplex coeff2 = make_cuDoubleComplex(h*h_theta*ALPHA_S_BAR / 2.0 / Pi, 0.0);
+
+		integrated[index] = cuCmul(integrated[index], coeff2);
+		
+	}
+}
+
+
 void Integration_in_BK_equation(std::complex<double>* Smatrix_in, std::complex<double>* Integrated_out)
 {
 
@@ -144,6 +427,62 @@ void Integration_in_BK_equation(std::complex<double>* Smatrix_in, std::complex<d
 	Integration_BK_direct <<<dimGrid, dimBlock >>> (Integrated_d, S_matrix_d, x_d, y_d, h, 0, N, N);
 
 	cudaMemcpy(Integrated_out, Integrated_d, sizeof(std::complex<double>)*N*N, cudaMemcpyDeviceToHost);
+
+
+	cudaFree(x_d);
+	cudaFree(y_d);;
+	cudaFree(Integrated_d);
+	cudaFree(S_matrix_d);
+	delete[](x);
+	delete[](y);
+}
+
+
+void Integration_in_logscale_BK_equation(std::complex<double>* Smatrix_in, std::complex<double>* Integrated_out)
+{
+
+
+	int N = NX;
+	double h = 1.0*LATTICE_SIZE / NX;
+	double h_theta = 2.0*Pi / NPHI;
+	double   xmax = h * NX / 1.0, xmin = -h * NX / 2.0, ymin = 0.0;
+	double   *x = new double[N*NPHI], *y = new double[N*NPHI];
+	for (int j = 0; j < NPHI; j++) {
+		for (int i = 0; i < NX; i++)
+		{
+			x[NX*j + i] = xmin + i * h;
+			y[NX*j + i] = ymin + j * h_theta;
+		}
+	}
+
+	// Allocate arrays on the device
+	double  *x_d, *y_d;
+	cudaMalloc((void**)&x_d, sizeof(double)*N*NPHI);
+	cudaMalloc((void**)&y_d, sizeof(double)*N*NPHI);
+	cudaMemcpy(x_d, x, sizeof(double)*N*NPHI, cudaMemcpyHostToDevice);
+	cudaMemcpy(y_d, y, sizeof(double)*N*NPHI, cudaMemcpyHostToDevice);
+
+	cuDoubleComplex *S_matrix_d;
+	cudaMalloc((void**)&S_matrix_d, sizeof(cuDoubleComplex) * N*NPHI);
+	cudaMemcpy(S_matrix_d, Smatrix_in, sizeof(std::complex<double>) * N*NPHI, cudaMemcpyHostToDevice);
+
+	cuDoubleComplex *Integrated_d;
+	cudaMalloc((void**)&Integrated_d, sizeof(cuDoubleComplex)*N*NPHI);
+
+	dim3 dimGrid(int((N - 0.5) / BSZ) + 1, int((NPHI - 0.5) / BSZ) + 1);
+	dim3 dimBlock(BSZ, BSZ);
+
+	//Integration_BK_logscale_direct_test <<<dimGrid, dimBlock >>> (Integrated_d, S_matrix_d, x_d, y_d, h, N);
+	Integration_BK_logscale_direct <<<dimGrid, dimBlock >>> (Integrated_d, S_matrix_d, x_d, y_d, h, N);
+
+	cudaError_t err = cudaGetLastError();
+	if (err != cudaSuccess) {
+		fprintf(stderr, "kernel launch failed: %s\n", cudaGetErrorString(err));
+		std::cout << "close the window" << '\n';
+		exit(-1);
+	}
+
+	cudaMemcpy(Integrated_out, Integrated_d, sizeof(std::complex<double>)*N*NPHI, cudaMemcpyDeviceToHost);
 
 
 	cudaFree(x_d);
