3e81f56816fdad4b4de2b9102c782233b32233c8
Hagiwara <hagiyoshihep@gmail.com>
2018-09-22 22:28:07 +0900

    add the integration in cpp program

--- Odderon_BK/RK_BK_including_odderon.cpp
+++ Odderon_BK/RK_BK_including_odderon.cpp
@@ -38,7 +38,7 @@ using namespace std;
 
 #define LARGE_Nc
 
-#define LOGSCALE
+//#define LOGSCALE
 
 //for DE formula
 int nfunc;
@@ -66,7 +66,7 @@ const double IMPACTP_B = 1.0;
 /**
 * The evolution step size.
 */
-#define DELTA_T         0.01
+#define DELTA_T         0.001
 #define OUTPUT_DELTA_T  0.1
 #define END_T           5.0
 
@@ -130,6 +130,306 @@ void init_BK_log(std::complex<double>* Smatrix_in) {
 }
 
 
+//input S_matrix NX*NPHI, x_1 NX, p_1 PHI;
+std::complex<double> linear_interpolation_Smatrix_cpp(std::complex<double>* S_matrix,
+	double* x_1, double* p_1, double x, double p)
+{
+	double h = 1.0*LATTICE_SIZE / NX;
+	double h_theta = 2.0*Pi / NPHI;
+	double   xmax = h * NX / 4.0, xmin = -h * NX *3.0 / 4.0, ymin = 0.0;
+	int i_x = 0;
+	int i_p = 0;
+
+	for (int i = 0; i < NX - 1; i++)
+	{
+		//double dx = x_1[i];
+		double dx = double(i)*h + xmin;
+		double diffdx = dx - x;
+		//printf("i %d \n", i);
+		if (0.0 > diffdx) {
+			i_x = i;
+			//printf(" if in dx - x %.3e ,i_x %d \n", diffdx , i_x);
+		}
+		else {
+			//printf("finish dx - x %.3e ,i_x %d \n", diffdx, i_x); 
+			continue;
+		}
+	}
+
+	for (int i = 0; i < NPHI - 1; i++)
+	{
+		//double dp = p_1[NX*i];
+		double dp = double(i)*h_theta;
+		double diffdp = dp - p;
+		if (0.0 > diffdp) { i_p = i; }
+		else { continue; }
+	}
+
+	if (x > x_1[NX - 1] || p > 2.0*Pi || x < x_1[0] || p < ymin)
+	{
+		printf("out of range \n");
+		assert(1);
+	}
+
+	//printf("i_x %d , i_p %d ,x_1[i_x] %.3e , p_1[i_p] %.3e , x %.3e , p %.3e \n" , i_x, i_p, x_1[i_x], p_1[NX*i_p], x, p);
+	//bilinear interpolation
+	double t = (x - x_1[i_x]) / (x_1[i_x + 1] - x_1[i_x]);
+	double u = (p - p_1[NX*i_p]) / (p_1[NX*(i_p + 1)] - p_1[NX*i_p]);
+
+	std::complex<double> interpolate;
+
+	interpolate = std::complex<double>(
+		(1.0 - t)*(1.0 - u)*S_matrix[i_p*NX + i_x].real() + t * (1.0 - u)*S_matrix[i_p*NX + i_x + 1].real()
+		+ t * u*S_matrix[(i_p + 1)*NX + i_x + 1].real() + (1.0 - t)*u*S_matrix[(i_p + 1)*NX + i_x].real(),
+		(1.0 - t)*(1.0 - u)*S_matrix[i_p*NX + i_x].imag() + t * (1.0 - u)*S_matrix[i_p*NX + i_x + 1].imag()
+		+ t * u*S_matrix[(i_p + 1)*NX + i_x + 1].imag() + (1.0 - t)*u*S_matrix[(i_p + 1)*NX + i_x].imag()
+		);
+
+	return interpolate;
+}
+
+
+void Integration_BK_direct_cpp(std::complex<double>* integrated, std::complex<double>* S_matrix,
+	double* x_1, double* y_1, double h, int N_ini, int N_las, int N) {
+
+
+#pragma omp parallel for num_threads(6)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int index = j * N + i;
+			integrated[index] = std::complex<double>(0.0, 0.0);
+			//sit the index which is center of the gaussian.
+
+			std::complex<double> complex_zero = std::complex<double>(0.0, 0.0);
+			//If x=N*j+i, then -x=N*(N-j)+N-i(when the origin is x= N*N/2 + N/2).
+			for (int m = N_ini; m < N_las; m++) {
+				for (int n = 0; n < N; n++) {
+					double simpson1 = 1.0;
+					double simpson2 = 1.0;
+					int diffinitm = m - N_ini;
+					if (m == N_ini || m == N_las - 1) {
+						simpson1 = 1.0 / 3.0;
+					}
+					else if (diffinitm % 2 == 0) {
+						simpson1 = 2.0 / 3.0;
+					}
+					else {
+
+						simpson1 = 4.0 / 3.0;
+					}
+
+
+					if (n == 0 || n == N - 1) {
+						simpson2 = 1.0 / 3.0;
+					}
+					else if (n % 2 == 0) {
+						simpson2 = 2.0 / 3.0;
+					}
+					else {
+
+						simpson2 = 4.0 / 3.0;
+					}
+
+					std::complex<double> trV_V = std::complex<double>(0.0, 0.0);
+					//if r-z is out of the region then we take the S(r-z) =0.
+					if ((j - m + N / 2) < 0 || (j - m + N / 2) > N - 1 || (i - n + N / 2) < 0 || (i - n + N / 2) > N - 1) {
+						//trV= - S(r)
+						trV_V = trV_V
+							- S_matrix[j * N + i];
+					}
+					else {
+						//trV=S(r-z)
+						trV_V = trV_V
+							+ S_matrix[(j - m + N / 2) * N + i - n + N / 2];
+						//trV=S(r-z)*S(-z) <- S(-x) = S(x)^*
+						trV_V = trV_V
+							* std::conj(S_matrix[m * N + n]);
+						//trV=S(r-z)*S(-z) - S(r)
+						trV_V = trV_V
+							- S_matrix[j * N + i];
+					}
+
+					std::complex<double> coeff = std::complex<double>(
+						simpson1*simpson2
+						*(x_1[j*N + i] * x_1[j*N + i] + y_1[j*N + i] * y_1[j*N + i])
+						/ ((x_1[m*N + n] - x_1[j*N + i])*(x_1[m*N + n] - x_1[j*N + i]) + (y_1[m*N + n] - y_1[j*N + i])*(y_1[m*N + n] - y_1[j*N + i]))
+						/ (x_1[m*N + n] * x_1[m*N + n] + y_1[m*N + n] * y_1[m*N + n]),
+						0.0
+						);
+
+					if (((x_1[m*N + n] - x_1[j*N + i])*(x_1[m*N + n] - x_1[j*N + i])
+						+ (y_1[m*N + n] - y_1[j*N + i])*(y_1[m*N + n] - y_1[j*N + i])) < 1.0e-12
+						|| (x_1[m*N + n] * x_1[m*N + n] + y_1[m*N + n] * y_1[m*N + n]) < 1.0e-12) {
+						coeff = std::complex<double>(0.0, 0.0);
+					}
+
+					integrated[index] = integrated[index] + coeff * trV_V;
+
+				}
+			}
+
+			std::complex<double> coeff2 = std::complex<double>(h*h*ALPHA_S_BAR / 2.0 / Pi, 0.0);
+
+			integrated[index] = integrated[index] * coeff2;
+		}
+	}
+}
+
+
+void Integration_BK_logscale_direct_cpp(std::complex<double>* integrated, std::complex<double>* S_matrix,
+	double* x_1, double* y_1, double h, int N) {
+
+#pragma omp parallel for num_threads(6)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < NPHI; j++) {
+			int index = j * N + i;
+			integrated[index] = std::complex<double>(0.0, 0.0);
+			//sit the index which is center of the gaussian.
+
+			std::complex<double> complex_zero = std::complex<double>(0.0, 0.0);
+			double   xmax = h * NX / 4.0, xmin = -h * NX* 3.0 / 4.0, ymin = 0.0;
+			double h_theta = 2.0*Pi / NPHI;
+			//If x=N*j+i, then -x=N*(N-j)+N-i(when the origin is x= N*N/2 + N/2).
+			for (int m = 0; m < N; m++) {
+				for (int n = 0; n < NPHI; n++) {
+					double simpson1 = 1.0;
+					double simpson2 = 1.0;
+
+
+					if (n == 0 || n == N - 1) {
+						simpson2 = 1.0 / 3.0;
+					}
+					else if (n % 2 == 0) {
+						simpson2 = 2.0 / 3.0;
+					}
+					else {
+
+						simpson2 = 4.0 / 3.0;
+					}
+
+					std::complex<double> trV_V = std::complex<double>(0.0, 0.0);
+
+					double r_z = 1.0 / 2.0*log(exp(2.0*x_1[j * N + i]) + exp(2.0*x_1[m * N + n])
+						- 2.0*exp(x_1[j * N + i] + x_1[m * N + n])*cos(y_1[j * N + i] - y_1[m * N + n]));
+					double r_z2 = exp(2.0*x_1[j * N + i]) + exp(2.0*x_1[m * N + n])
+						- 2.0*exp(x_1[j * N + i] + x_1[m * N + n])*cos(y_1[j * N + i] - y_1[m * N + n]);
+					double angletocos = (exp(x_1[j * N + i])*cos(y_1[j * N + i]) - exp(x_1[m * N + n]) * cos(y_1[m * N + n])) / sqrt(r_z2);
+					if (angletocos >= 1.0) {
+						//printf("cos is larger than 1 cos if %.6e\n" , angletocos-1.0);
+						angletocos = 1.0;
+					}
+					else if (angletocos <= -1.0) {
+						//printf("cos is smaller than -1 cos if %.6e\n", angletocos+1.0);
+						angletocos = -1.0;
+					}
+
+					//if r-z is out of the region then we take the S(r-z) =0.
+					if ((j - m + N / 2) < 0 || (j - m + N / 2) > N - 1 || (i - n + N / 2) < 0 || (i - n + N / 2) > N - 1) {
+						//trV= - S(r)
+						trV_V = trV_V
+							- S_matrix[j * N + i];
+					}
+					else {
+						//trV=S(r-z)
+						trV_V = trV_V
+							+ linear_interpolation_Smatrix_cpp(S_matrix, x_1, y_1, r_z,
+								acos(angletocos));
+						//trV=S(r-z)*S(-z) <- S(-x) = S(x)^*
+						trV_V = trV_V
+							* std::conj(S_matrix[m * N + n]);
+						//trV=S(r-z)*S(-z) - S(r)
+						trV_V = trV_V
+							- S_matrix[j * N + i];
+					}//Caution!!! nan * 0 = nan
+					if (((x_1[j * N + i] - x_1[m * N + n])*(x_1[j * N + i] - x_1[m * N + n]) < 1.0e-10 &&
+						(y_1[j * N + i] - y_1[m * N + n])*(y_1[j * N + i] - y_1[m * N + n]) < 1.0e-10) || r_z2 < 0.0) {
+						trV_V = std::complex<double>(
+							0.0
+							,
+							0.0
+							);
+					}
+
+					std::complex<double> coeff = std::complex<double>(
+						simpson1*simpson2
+						*exp(2.0*x_1[j*N + i])
+						/ r_z2,
+						0.0
+						);
+
+					if (((x_1[m*N + n] - x_1[j*N + i])*(x_1[m*N + n] - x_1[j*N + i])
+						+ (y_1[m*N + n] - y_1[j*N + i])*(y_1[m*N + n] - y_1[j*N + i])) < 1.0e-12
+						|| (x_1[m*N + n] * x_1[m*N + n] + y_1[m*N + n] * y_1[m*N + n]) < 1.0e-12) {
+						coeff = std::complex<double>(0.0, 0.0);
+					}
+
+					integrated[index] = integrated[index] + coeff * trV_V;
+
+				}
+			}
+
+			std::complex<double> coeff2 = std::complex<double>(h*h_theta*ALPHA_S_BAR / 2.0 / Pi, 0.0);
+
+			integrated[index] = integrated[index] * coeff2;
+		}
+	}
+}
+
+
+void Integration_in_BK_equation_cpp(std::complex<double>* Smatrix_in, std::complex<double>* Integrated_out)
+{
+
+
+	int N = NX;
+	double h = 1.0*LATTICE_SIZE / NX;
+	double   xmax = h * NX / 2.0, xmin = -h * NX / 2.0, ymin = -h * NX / 2.0;
+	double   *x = new double[N*N], *y = new double[N*N];
+	for (int j = 0; j < NX; j++) {
+		for (int i = 0; i < NX; i++)
+		{
+			x[NX*j + i] = xmin + i * h;
+			y[NX*j + i] = ymin + j * h;
+		}
+	}
+
+
+	Integration_BK_direct_cpp(Integrated_out, Smatrix_in, x, y, h, 0, N, N);
+
+
+
+	delete[](x);
+	delete[](y);
+}
+
+
+void Integration_in_logscale_BK_equation_cpp(std::complex<double>* Smatrix_in, std::complex<double>* Integrated_out)
+{
+
+
+	int N = NX;
+	double h = 1.0*LATTICE_SIZE / NX;
+	double h_theta = 2.0*Pi / NPHI;
+	double   xmax = h * NX / 4.0, xmin = -h * NX* 3.0 / 4.0, ymin = 0.0;
+	double   *x = new double[N*N], *y = new double[N*N];
+	for (int j = 0; j < NX; j++) {
+		for (int i = 0; i < NX; i++)
+		{
+			x[NX*j + i] = xmin + i * h;
+			y[NX*j + i] = ymin + j * h_theta;
+		}
+	}
+
+
+	Integration_BK_logscale_direct_cpp(Integrated_out, Smatrix_in, x, y, h, N);
+
+
+
+	delete[](x);
+	delete[](y);
+}
+
+
+
 void Integration_in_BK_equation(std::complex<double>* Smatrix_in, std::complex<double>* Integrated_out);
 
 
@@ -145,6 +445,7 @@ void f_one_step_BK_complex(
 	}
 
 	Integration_in_BK_equation(temp_s_BMS_in.data(), NEW_BMS_g_in.data());
+	//Integration_in_BK_equation_cpp(temp_s_BMS_in.data(), NEW_BMS_g_in.data());
 
 }
 
@@ -160,6 +461,7 @@ void s_one_step_BK_complex(const vector<complex<double>> &K1_g_in,
 	}
 
 	Integration_in_BK_equation(temp_s_BMS_in.data(), NEW_BMS_g_in.data());
+	//Integration_in_BK_equation_cpp(temp_s_BMS_in.data(), NEW_BMS_g_in.data());
 
 }
 
